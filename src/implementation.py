8d92ee902bc53cc906170b3d856b4f66c3f93d05
Dylan James <djame011@gold.ac.uk>
2022-03-17 16:21:42 +0000

    Commented out live tests, changed the URL for get_request mock tests, added test for create_json and made it green

--- src/implementation.py
+++ src/implementation.py
@@ -4,3 +4,6 @@ def get_request(url):
   r = requests.get(url)
   custom_dictionary = {"URL": r.url, "Status-code": r.status_code, "Content-length": int(r.headers['Content-Length'])}
   return custom_dictionary
+
+def create_json(string):
+  return [string]
--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -3,18 +3,22 @@ from unittest.mock import Mock
 
 #Tests
 
-##live implementation tests
+##Live implementation tests
 
-def test_live_get_request():
-  assert implementation.get_request('http://checkip.amazonaws.com/')["URL"] == 'http://checkip.amazonaws.com/'
-  assert implementation.get_request('http://checkip.amazonaws.com/')["Status-code"] == 200
-  assert implementation.get_request('http://checkip.amazonaws.com/')["Content-length"] == 15
+# def test_live_get_request():
+#   assert implementation.get_request('http://checkip.amazonaws.com/')["URL"] == 'http://checkip.amazonaws.com/'
+#   assert implementation.get_request('http://checkip.amazonaws.com/')["Status-code"] == 200
+#   assert implementation.get_request('http://checkip.amazonaws.com/')["Content-length"] == 15
 
 ##Mock implementation tests
 
 def test_mock_get_request(): 
-  implementation_mock = Mock()
-  implementation_mock.get_request.return_value = {"URL": 'http://checkip.amazonaws.com/', "Status-code": 200, "Content-length": 15}
+  get_request_mock = Mock()
+  get_request_mock.get_request.return_value = {"URL": 'http://google.com/', "Status-code": 200, "Content-length": 15}
 
-  assert implementation_mock.get_request()["URL"] == 'http://checkip.amazonaws.com/'
-  
+  assert get_request_mock.get_request()["URL"] == 'http://google.com/'
+
+def test_mock_create_json():
+  string = 'http://google.com/'
+
+  assert len(implementation.create_json(string)) == 1
