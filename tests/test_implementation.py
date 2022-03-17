0bcdaccccace094eeb1f91caf4580790fb8ed108
Dylan James <djame011@gold.ac.uk>
2022-03-17 15:34:31 +0000

    Added tests for Mocking get_request method and changed test names

--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -1,6 +1,20 @@
 import src.implementation as implementation
+from unittest.mock import Mock
 
-def test_implmentation():
+#Tests
+
+##live implementation tests
+
+def test_live_get_request():
   assert implementation.get_request('http://checkip.amazonaws.com/')["URL"] == 'http://checkip.amazonaws.com/'
   assert implementation.get_request('http://checkip.amazonaws.com/')["Status-code"] == 200
   assert implementation.get_request('http://checkip.amazonaws.com/')["Content-length"] == 15
+
+##Mock implementation tests
+
+def test_mock_get_request(): 
+  implementation_mock = Mock()
+  implementation_mock.get_request.return_value = {"URL": 'http://checkip.amazonaws.com/', "Status-code": 200, "Content-length": 15}
+
+  assert implementation_mock.get_request()["URL"] == 'http://checkip.amazonaws.com/'
+  
