b4969bf1d768a1c1c11ceaf3626b4f0cc4cfa39f
Dylan James <djame011@gold.ac.uk>
2022-03-21 12:55:54 +0000

    Working implementation file

--- src/implementation.py
+++ src/implementation.py
@@ -1,13 +1,23 @@
 import requests
+import json
 
 def get_request(url):
   r = requests.get(url)
   custom_dictionary = {"URL": r.url, "Status-code": r.status_code, "Content-length": int(r.headers['Content-Length'])}
-  return custom_dictionary
+  print(json.dumps(custom_dictionary, indent=2))
 
 def create_json(string):
   websites = string.splitlines()
-  websites_array = []
   for item in websites:
-    websites_array.append(item)
-  return websites_array
+    get_request(item)
+
+print("Type website URLs to get the JSON, finish your input with Ctrl-D:")
+user_input = ''
+while True:
+  try:
+    user_input += (input() + '\n')
+  except EOFError:
+    print("Results:")
+    break
+
+create_json(user_input)
